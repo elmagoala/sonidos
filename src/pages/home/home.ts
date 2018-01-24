0e6b568a555e48c5f692d96cc38bfe5cbc0fe8a3
homero <homero@correo.com>
2018-01-24 15:13:52 -0500

    se implementa la eliminacion de la lista de animales

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,8 +9,39 @@ import { Animal } from '../../interfaces/animal.interface';
 export class HomePage {
 
   animales:Animal[] = [];
+  audio = new Audio();
+  audioTiempo:any;
   constructor() {
-    this.animales = ANIMALES.splice(0);
+    this.animales = ANIMALES.slice(0);
+  }
+
+  reproducir( animal:Animal ) {
+    this.pausarAudio(animal);
+    if(animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+    }
+    this.audio.src = animal.audio;
+    this.audio.load();
+    this.audio.play();
+    animal.reproduciendo = true;
+    this.audioTiempo = setTimeout( ()=> animal.reproduciendo=false,
+                                        animal.duracion*1000 );
+  }
+
+  borrarAnimal(index:number){
+    this.animales.splice(index,1);
+  }
+
+  private pausarAudio( animalSelec:Animal ) {
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+    for(let animal of this.animales){
+      if(animalSelec.nombre != animal.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
   }
 
 }
